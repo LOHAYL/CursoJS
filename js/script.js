0c221d46fa65a3057486fd02e9e4b139295f9191
Leandro Lohay <leandrolohay13@gmail.com>
2023-10-31 17:11:07 -0300

    Primer Commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+
+function menu(){
+
+    let bandera = true
+    let totalAbonar = 0
+    let contador = 0
+
+    do{
+
+        let opcion = parseInt(prompt("Seleccione el producto que desea comprar: \n1)Lima Negra Super ($469)\n2)Lima Block Negra ($543)\n3)Esmalte Semipermanente ($1885)\n4)Removedor de Cuticulas ($2731)\n5)Escofia de Metal Premium ($2867)\n6)Salir\n\nTotal de proeductos en el carrito: "+contador))
+
+        switch (opcion){
+            case 1: totalAbonar += 469
+            contador++
+            break
+            case 2: totalAbonar += 543
+            contador++
+            break
+            case 3: totalAbonar += 1885
+            contador++
+            break
+            case 4: totalAbonar += 2731
+            contador++
+            break
+            case 5: totalAbonar += 2867
+            contador++
+            break
+            case 6: bandera = false
+            break
+            default : alert("Seleccione una opcion valida")
+            break
+        }
+
+
+    }while (bandera)
+
+    
+    if(totalAbonar != 0){
+
+        if(confirm("¿Tienes algun codigo de descuento?")){
+
+            alert("Total que debe abonar: $"+descuento(totalAbonar))
+
+        }else alert("Total que debes abonar: $"+totalAbonar)
+        
+    }
+        
+}
+
+function descuento(total){
+
+    let codigo = prompt("Ingresa el codigo de descuento: ")
+
+    if(codigo === "VIP2023"){
+        total = total - (total*20)/100
+        return total
+
+    }else{
+        alert("Codigo Incorrecto")
+        return total
+    }
+
+}
+
+
+menu()
+
