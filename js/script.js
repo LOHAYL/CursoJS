3b7e890464a196eb0c069368c8a6ee13ae8b62e1
Leandro Lohay <leandrolohay13@gmail.com>
2023-11-01 13:14:12 -0300

    Se agrego la funcion de mostrar el subtotal del carrito y lo que se ahorro

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function menu(){
 
     do{
 
-        let opcion = parseInt(prompt("Seleccione el producto que desea comprar: \n1)Lima Negra Super ($469)\n2)Lima Block Negra ($543)\n3)Esmalte Semipermanente ($1885)\n4)Removedor de Cuticulas ($2731)\n5)Escofia de Metal Premium ($2867)\n6)Salir\n\nTotal de productos en el carrito: "+contador))
+        let opcion = parseInt(prompt("Seleccione el producto que desea comprar: \n1)Lima Negra Super ($469)\n2)Lima Block Negra ($543)\n3)Esmalte Semipermanente ($1885)\n4)Removedor de Cuticulas ($2731)\n5)Escofia de Metal Premium ($2867)\n6)Salir\nTotal de productos en el carrito: "+contador+"\nTotal a abonar: $"+totalAbonar))
 
         switch (opcion){
             case 1: totalAbonar += 469
@@ -40,7 +40,10 @@ function menu(){
 
         if(confirm("¿Tienes algun codigo de descuento?")){
 
-            alert("Total que debe abonar: $"+descuento(totalAbonar))
+            let aplicarDescuento = parseInt(descuento(totalAbonar))
+            
+            alert("Total que debe abonar: $"+aplicarDescuento+"\n\nUsted ahorro: $"+(totalAbonar-aplicarDescuento))
+            
 
         }else alert("Total que debes abonar: $"+totalAbonar)
         
