039615dc6378aae8ab2c2ecd1ac33c8777c3db21
Leandro Lohay <leandrolohay13@gmail.com>
2023-11-02 16:18:13 -0300

    Se agrego la funcion de terminar la compra con el boton Cancelar

--- js/script.js
+++ js/script.js
@@ -7,25 +7,25 @@ function menu(){
 
     do{
 
-        let opcion = parseInt(prompt("Seleccione el producto que desea comprar: \n1)Lima Negra Super ($469)\n2)Lima Block Negra ($543)\n3)Esmalte Semipermanente ($1885)\n4)Removedor de Cuticulas ($2731)\n5)Escofia de Metal Premium ($2867)\n6)Salir\nTotal de productos en el carrito: "+contador+"\nTotal a abonar: $"+totalAbonar))
+        let opcion = prompt("Seleccione el producto que desea comprar: \n1)Lima Negra Super ($469)\n2)Lima Block Negra ($543)\n3)Esmalte Semipermanente ($1885)\n4)Removedor de Cuticulas ($2731)\n5)Escofia de Metal Premium ($2867)\nPrecione CANCELAR para terminar la compra\nTotal de productos en el carrito: "+contador+"\nTotal a abonar: $"+totalAbonar)
 
         switch (opcion){
-            case 1: totalAbonar += 469
+            case "1": totalAbonar += 469
             contador++
             break
-            case 2: totalAbonar += 543
+            case "2": totalAbonar += 543
             contador++
             break
-            case 3: totalAbonar += 1885
+            case "3": totalAbonar += 1885
             contador++
             break
-            case 4: totalAbonar += 2731
+            case "4": totalAbonar += 2731
             contador++
             break
-            case 5: totalAbonar += 2867
+            case "5": totalAbonar += 2867
             contador++
             break
-            case 6: bandera = false
+            case null: bandera = false
             break
             
             default : alert("Seleccione una opcion valida")
